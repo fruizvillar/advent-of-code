9cc11f974d7c195d4a29bca48c6e9b707a75b08a
frrv <frvillar@gmv.com>
2022-12-09 18:19:25 +0100

    Day07 .001% done

---
+++ 2022/07.py
@@ -0,0 +1,20 @@
+import heapq
+
+from problem import AOCProblem
+
+
+class TodaysProblem(AOCProblem):
+    N = 7
+
+    def solve1(self, buffer):
+        for line in buffer.readlines():
+            pass
+
+    def solve2(self, buffer):
+        return None
+
+
+
+
+if __name__ == '__main__':
+    TodaysProblem(test_input=)()
