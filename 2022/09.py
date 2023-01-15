ff42430b196c463fe8d3d8fd7aa3aa58f3175f94
francisco <franciscoruizvillar@gmail.com>
2023-01-15 12:37:41 +0100

    Day 09 star 1

--- 2022/09.py
+++ 2022/09.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from problem import AOCProblem, Direction2D
+from problem import AOCProblem, Direction2D, Coordinate2D
 
 
 class TodaysProblem(AOCProblem):
@@ -9,18 +9,62 @@ class TodaysProblem(AOCProblem):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.movements = []
+        self.head = Coordinate2D()
+        self.tail = Coordinate2D()
+        self._tail_histo = {self.tail}
 
     def load_data(self, f: Path):
         with f.open() as buffer:
             for line in buffer.readlines():
                 segments = line.split()
-                self.movements.append((Direction2D[segments[0]], segments[1]))
+                self.movements.append((Direction2D[segments[0]], int(segments[1])))
 
     def solve1(self):
-        print(self)
+        for direction, amount in self.movements:
+            for _ in range(amount):
+                self.head += direction
+                self._make_tail_follow_head()
+
+        return len(self._tail_histo)
+
 
     def solve2(self):
         pass
+
+    def _make_tail_follow_head(self):
+        if self.head.chess_king_distance(self.tail) <= 1:
+            return
+
+        diff = self.head - self.tail
+
+        diff_1_step = diff.to_king_move()
+        new_tail = self.tail + diff_1_step
+
+        self.tail = new_tail
+        self._tail_histo.add(new_tail)
+
+    def _plot(self):
+        y0 = min(self.head.y, self.tail.y, 0)
+        yf = max(self.head.y, self.tail.y, 0)
+        x0 = min(self.head.x, self.tail.x, 0)
+        xf = max(self.head.x, self.tail.x, 0)
+
+        for y in range(y0, yf+1):
+            for x in range(x0, xf+1):
+                if (y, x) == self.head and self.head == self.tail:
+                    c = 'X'
+                elif (y, x) == self.head:
+                    c = 'H'
+                elif (y, x) == self.tail:
+                    c = 'T'
+                elif not x and not y:
+                    c = 'S'
+                else:
+                    c = '.'
+
+                print(c, end='')
+
+            print()
 
     def __str__(self):
         return '\n'.join((' '.join(str(x) for x in row) for row in self.movements))
--- 2022/problem.py
+++ 2022/problem.py
@@ -1,7 +1,8 @@
 import abc
 from enum import Enum
-from typing import Literal
 from pathlib import Path
+from typing import Literal
+
 
 class Direction2D(Enum):
     """Indicates any direction in a 2D grid (unitary vector)"""
@@ -19,6 +20,70 @@ class Direction2D(Enum):
     def x(self) -> Literal[-1, 0, 1]:
         """ X coordinate of the direction"""
         return self.value[1]
+
+
+class Coordinate2D:
+
+    def __init__(self, y=0, x=0):
+
+        if isinstance(y, (Coordinate2D, Direction2D)):
+            self.y = y.y
+            self.x = y.x
+            return
+
+        if isinstance(y, tuple):
+            self.y, self.x = y
+            return
+
+        self.y = y
+        self.x = x
+
+    @property
+    def as_tuple(self) -> tuple:
+        return self.y, self.x
+
+    def __add__(self, other) -> 'Coordinate2D':
+        if isinstance(other, (Coordinate2D, Direction2D)):
+            return Coordinate2D(self.y + other.y, self.x + other.x)
+
+        if isinstance(other, tuple) and len(other) == 2:
+            return Coordinate2D(self.y + other[0], self.x + other[1])
+
+        raise NotImplementedError
+
+    def __neg__(self) -> 'Coordinate2D':
+        return Coordinate2D(-self.y, -self.x)
+
+    def __sub__(self, other) -> 'Coordinate2D':
+        return self.__add__(other.__neg__())
+
+    def __eq__(self, other):
+        if isinstance(other, Coordinate2D):
+            return self.x == other.x and self.y == other.y
+        if isinstance(other, tuple):
+            return self.as_tuple == other
+
+    def __str__(self):
+        return f'({self.y: d}, {self.x: d})'
+
+    def chess_king_distance(self, other):
+        other = Coordinate2D(other)
+
+        abs_y = abs(self.y - other.y)
+        abs_x = abs(self.x - other.x)
+
+        return max(abs_y, abs_x)  # Diagonal moves would make the lower component irrelevant
+
+    def to_king_move(self) -> 'Coordinate2D':
+        mod_y = self.y // abs(self.y) if self.y else 0
+        mod_x = self.x // abs(self.x) if self.x else 0
+        return Coordinate2D(mod_y, mod_x)
+
+    def __hash__(self):
+        return self.as_tuple.__hash__()
+
+    def __lt__(self, other):
+        return self.as_tuple.__lt__(other.as_tuple)
 
 
 class AOCProblem(abc.ABC):
